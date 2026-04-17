004a2a08a8a33a91e385a2f29b9b0ba52ab94050
EmperorXSama <5alidghoddane@protonmail.com>
2026-04-17 19:44:14 +0100

    Configuring and applying database setup : -structure a database folder that containes ApplicationDbContext.cs + Entity folder with initial data structure needed - configure and setup postgres database in DependencyInjection.cs -add Extension Method for auto migration ( need to be changed in production) - checking grafana for data flow ( status = working )

---
+++ HabitDev/Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using System.Reflection;
+using HabitDev.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabitDev.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+
+    public DbSet<Habit> Habits { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
---
+++ HabitDev/Database/Schemas.cs
@@ -0,0 +1,6 @@
+﻿namespace HabitDev.Database;
+
+public static class Schemas
+{
+    public const string Application = "dev_habit";
+}
